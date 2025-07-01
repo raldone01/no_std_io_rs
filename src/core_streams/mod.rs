a7ba8b9bb189fd9cb83e23a82240186f68a20209
raldone01 <raldone01@gmail.com>
2025-07-01 16:54:01 +0200

    Rename.

--- src/core_streams/mod.rs
+++ src/core_streams/mod.rs
@@ -1,19 +1,19 @@
-mod bi_cursor;
-mod bi_empty;
 mod reader_buffered;
 mod reader_bytewise;
 mod reader_forked_buffered;
 mod reader_limited;
+mod rw_cursor;
+mod rw_empty;
 mod writer_buffered;
 mod writer_bytewise;
 mod writer_limited;
 
-pub use bi_cursor::*;
-pub use bi_empty::*;
 pub use reader_buffered::*;
 pub use reader_bytewise::*;
 pub use reader_forked_buffered::*;
 pub use reader_limited::*;
+pub use rw_cursor::*;
+pub use rw_empty::*;
 pub use writer_buffered::*;
 pub use writer_bytewise::*;
 pub use writer_limited::*;
